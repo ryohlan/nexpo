45687289ec1322d29bbd5a097e526fb6c3a512a4
ryohlan <sunsunryohlan@gmail.com>
2018-11-09 17:05:53 +0900

    add next.js

--- HelloWorld.tsx
+++ HelloWorld.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Platform } from "react-native";
 
 export const HelloWorld = () => (
   <View style={{ padding: 10 }}>
-    <Text style={{ fontSize: 40 }}>Hello World</Text>
+    <Text style={{ fontSize: 40 }}>Hello World. This env is {Platform.OS}</Text>
   </View>
 );
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,12 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    env: {
+      web: {
+        presets: ["next/babel"],
+        plugins: [["react-native-web", { commonjs: true }]]
+      }
+    }
   };
 };
