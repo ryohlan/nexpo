d4cfdf1b5642e1686bd37d3f9d6014b5589a514f
ryohlan <sunsunryohlan@gmail.com>
2018-11-09 16:43:28 +0900

    typescript configuration

---
+++ HelloWorld.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export const HelloWorld = () => (
+  <View style={{ padding: 10 }}>
+    <Text style={{ fontSize: 40 }}>Hello World</Text>
+  </View>
+);
