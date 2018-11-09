5b3c2fbc54481a82a898e350ceb5d76e83b4da7e
ryohlan <sunsunryohlan@gmail.com>
2018-11-10 02:15:18 +0900

    add custom forms

--- src/common/screens/HelloWorld.tsx
+++ src/common/screens/HelloWorld.tsx
@@ -1,10 +1,13 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, TextInput, Alert } from "react-native";
 
 export const HelloWorld = () => (
   <Wrapper>
     <HelloWorldText />
-    <LoginButton />
+    <BasicTextForm />
+    <PasswordForm />
+    <SignUpButton onPress={() => Alert.alert("hogehoge")} />
+    <LoginButton onPress={() => console.log("login")} />
   </Wrapper>
 );
 
@@ -36,13 +39,64 @@ const LoginButton = (props: any) => (
       backgroundColor: "#333",
       paddingVertical: 12,
       paddingHorizontal: 16,
-      width: 200,
+      width: 240,
       borderRadius: 4,
-      marginTop: 20
+      marginVertical: 10
     }}
   >
     <Text style={{ fontWeight: "bold", color: "#FFF", fontSize: 18 }}>
       LOGIN
     </Text>
   </TouchableOpacity>
+);
+
+const SignUpButton = (props: any) => (
+  <TouchableOpacity
+    {...props}
+    style={{
+      backgroundColor: "#333",
+      paddingVertical: 12,
+      paddingHorizontal: 16,
+      width: 240,
+      borderRadius: 4,
+      marginVertical: 10
+    }}
+  >
+    <Text style={{ fontWeight: "bold", color: "#FFF", fontSize: 18 }}>
+      SIGN UP
+    </Text>
+  </TouchableOpacity>
+);
+
+const BasicTextForm = (props: any) => (
+  <TextInput
+    {...props}
+    style={{
+      borderBottomColor: "#333",
+      borderBottomWidth: 1,
+      width: 240,
+      padding: 8,
+      fontSize: 16,
+      marginVertical: 10
+    }}
+    placeholder="Input your email"
+    placeholderTextColor="rgba(0,0,0,0.3)"
+  />
+);
+
+const PasswordForm = (props: any) => (
+  <TextInput
+    {...props}
+    style={{
+      borderBottomColor: "#333",
+      borderBottomWidth: 1,
+      width: 240,
+      padding: 8,
+      fontSize: 16,
+      marginVertical: 10
+    }}
+    placeholder="Input your password"
+    placeholderTextColor="rgba(0,0,0,0.3)"
+    secureTextEntry
+  />
 );
